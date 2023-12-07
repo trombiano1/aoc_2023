ba116a94d2786e1bcded98647b1af8cfa69119fa
trombiano1 <trombiano1@gmail.com>
2023-12-07 19:13:34 +0900

    day 7

---
+++ advent_7_2/src/main.rs
@@ -0,0 +1,84 @@
+use std::io::{self, BufRead};
+use std::collections::HashMap;
+
+fn main() {
+    let stdin = io::stdin();
+    let reader = stdin.lock();
+
+    let mut mp: HashMap<char, usize> = HashMap::new();
+    let char_strength = "J23456789TQKA";
+    for (i, c) in char_strength.chars().enumerate() {
+        mp.insert(c, i);
+    }
+
+    let mut cards: Vec<Vec<(String, i32)>> = vec![vec![]; 7];
+    for line in reader.lines() {
+        match line {
+            Ok(line) => {
+                let mut parts = line.split(" ");
+                let card = parts.next().unwrap().to_string();
+                let bid: i32 = parts.next().unwrap().parse().unwrap();
+                
+                let mut alph = vec![0; 13];
+                let mut jokers = 0;
+
+                for c in card.chars() {
+                    if c == 'J' {
+                        jokers += 1;
+                        continue;
+                    }
+                    alph[mp[&c]] += 1;
+                }
+                
+                let mut max = 0;
+                let mut second_max = 0;
+                for a in alph {
+                    if a > max {
+                        second_max = max;
+                        max = a;
+                    } else if a > second_max {
+                        second_max = a;
+                    }
+                }
+
+                max += jokers;
+                
+                let idx = |max, second_max| match (max, second_max) {
+                    (5, _) => 6 as usize,
+                    (4, _) => 5 as usize,
+                    (3, 2) => 4 as usize,
+                    (3, 1) => 3 as usize,
+                    (2, 2) => 2 as usize,
+                    (2, 1) => 1 as usize,
+                    (1, _) => 0 as usize,
+                    _ => panic!("err"),
+                };
+
+                cards[idx(max, second_max)].push((card, bid));
+            }
+            Err(e) => {
+                eprintln!("Error: {}", e);
+                break;
+            }
+        }
+    }
+    
+    let mut c = 0;
+    let mut ans: i64 = 0;
+    for mut card_type in cards {
+        card_type.sort_by(|i, j| {
+            for (ci, cj) in i.0.chars().zip(j.0.chars()) {
+                if ci != cj {
+                    return mp[&ci].cmp(&mp[&cj]);
+                }
+            }
+            std::cmp::Ordering::Equal
+        });
+        for card in card_type {
+            c += 1;
+            ans += card.1 as i64 * c as i64;
+        }
+    }
+
+    println!("{}", ans);
+}
