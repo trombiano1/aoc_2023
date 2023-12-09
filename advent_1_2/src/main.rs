d4eca0c53ec181fee216df5a8993d9a0e21fa347
trombiano1 <trombiano1@gmail.com>
2023-12-09 17:49:32 +0900

    day 9

--- advent_1_2/src/main.rs
+++ advent_1_2/src/main.rs
@@ -1,29 +1,26 @@
-use proconio::input;
+use std::io::{self, BufRead};
 
 fn main() {
+    let stdin = io::stdin();
+    let reader = stdin.lock();
+
     let nums: Vec<String> = vec!["one".to_string(), "two".to_string(), "three".to_string(),
                                "four".to_string(), "five".to_string(), "six".to_string(),
                                "seven".to_string(), "eight".to_string(), "nine".to_string()];
     let mut ans = 0;
-    loop {
-        input! {
-            line: String,
-        }
+    for line in reader.lines() {
+        let dat = line.unwrap();
 
-        if line.is_empty() {
-            break;
-        }
+        let mut a = 0;
+        let mut b = 0;
 
-        let mut a: Option<u32> = None;
-        let mut b: Option<u32> = None;
-        
-        for (i, c) in line.chars().enumerate() {
+        for (i, c) in dat.chars().enumerate() {
             if c.is_digit(10) {
                 let digit = c.to_digit(10).unwrap();
-                if a.is_none() {
-                    a = Some(digit);
+                if a == 0 {
+                    a = digit;
                 }
-                b = Some(digit);
+                b = digit;
             }
             
             for (k, num) in nums.iter().enumerate() {
@@ -32,24 +29,22 @@ fn main() {
                 }
                 let mut flag = true;
                 for j in 0..num.len() {
-                    if line.as_bytes()[i + 1 - num.len() + j] != num.as_bytes()[j] {
+                    if dat.as_bytes()[i + 1 - num.len() + j] != num.as_bytes()[j] {
                         flag = false;
                         break;
                     }
                 }
                 if flag {
                     let digit = (k + 1) as u32;
-                    if a.is_none() {
-                        a = Some(digit);
+                    if a == 0 {
+                        a = digit;
                     }
-                    b = Some(digit);
+                    b = digit;
                 }
             }
             
         }
-        if let (Some(a_val), Some(b_val)) = (a, b) {
-            ans += a_val * 10 + b_val;
-        }
+        ans += a * 10 + b;
     }
     println!("{}", ans);
 }
